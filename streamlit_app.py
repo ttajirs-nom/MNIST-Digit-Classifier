e751eb759e52c7716908e435bd7f59d9884dcd0d
ttajirs-nom <t.tajirs@gmail.com>
2026-06-24 11:33:34 +0500

    Removed FastAPI and added direct TensorFlow inference

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,45 @@
 import streamlit as st
-import requests
 from PIL import Image
 import numpy as np
+from tensorflow.keras.models import load_model
+
+# Load model
+model = load_model("models/mnist_ann_model.h5")
 
 st.title("MNIST Digit Classifier (ANN)")
-
 st.write("Upload a handwritten digit image (0–9)")
 
-uploaded_file = st.file_uploader("Choose an image", type=["png", "jpg", "jpeg"])
+uploaded_file = st.file_uploader(
+    "Choose an image",
+    type=["png", "jpg", "jpeg"]
+)
 
 if uploaded_file is not None:
 
     image = Image.open(uploaded_file)
 
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", width=250)
 
     image = image.convert("L")
     image = image.resize((28, 28))
 
     img_array = np.array(image)
 
-    # invert colors (VERY IMPORTANT for MNIST)
+    # Invert colors for MNIST
     img_array = 255 - img_array
 
     img_array = img_array / 255.0
 
-    # Flatten
-    img_array = img_array.flatten().tolist()
+    # Flatten to 784 features
+    img_array = img_array.reshape(1, 784)
 
     if st.button("Predict Digit"):
 
-        response = requests.post(
-            "http://127.0.0.1:8000/predict",
-            json={"image": img_array}
-        )
+        prediction = model.predict(img_array)
 
-        result = response.json()
+        digit = np.argmax(prediction)
+        confidence = np.max(prediction) * 100
 
         st.subheader("Prediction Result")
-
-        st.write(result)
+        st.write(f"Predicted Digit: {digit}")
+        st.write(f"Confidence: {confidence:.2f}%")
